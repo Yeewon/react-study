eff3d3419cd509cef4092fca259e11f2aee5ff0f
정예원 <yes3427@gmail.com>
2021-11-26 00:23:13 +0900

    ExpensesFilter 컴포넌트 구현

--- react-study/src/components/Expenses/Expenses.js
+++ react-study/src/components/Expenses/Expenses.js
@@ -1,16 +1,29 @@
 import ExpenseItem from "./ExpenseItem";
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      {props.items.map(({ id, title, amount, date }) => {
-        return (
-          <ExpenseItem key={id} title={title} amount={amount} date={date} />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          seleted={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map(({ id, title, amount, date }) => {
+          return (
+            <ExpenseItem key={id} title={title} amount={amount} date={date} />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
