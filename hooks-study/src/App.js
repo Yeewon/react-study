9e4bd504008f7f3efd290a8e6a109ff3d7cb3ed7
정예원 <yes3427@gmail.com>
2021-12-01 04:20:26 +0900

    useInput hook 구현

--- hooks-study/src/App.js
+++ hooks-study/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import useInput from "./hooks/useInput";
 
 function App() {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mrs.", maxLen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder="name" {...name} />
     </div>
   );
 }
